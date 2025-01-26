12f2133450b8e9b74a12eff0a4b6cb5ddacb585e
Julian Andres Pereira <jpereirap@unal.edu.co>
2025-01-26 17:01:57 -0500

    Side Menu scratch done

--- src/components/Menu/SideMenu.styled.tsx
+++ src/components/Menu/SideMenu.styled.tsx
@@ -1,27 +1,68 @@
-import { Box, Icon, styled } from "@mui/material";
+import { Box, styled } from "@mui/material";
 
 export const SideMenuContainer = styled(Box)<{ active: boolean }>(
   ({ active }) => ({
+    display: "flex",
+    flexDirection: "column",
     position: "absolute",
     backgroundColor: "#2864f6",
     color: "white",
     borderRadius: "0px 15px 15px 0px",
-    width: active ? "150px" : "50px",
+    width: active ? "200px" : "50px",
     height: "calc(100% - 10px)",
     margin: "5px",
     transition: "width 0.2s ease-in",
   })
 );
 
-export const MenuOption = styled(Box)({
+export const MenuHeader = styled(Box)({
+  marginBottom: "1rem",
+});
+
+export const MenuSection = styled("section")({
+  marginTop: "1rem",
+});
+
+export const MenuHr = styled("hr")({
+  margin: "0px 10px",
+  height: "2px",
+  backgroundColor: "#3872f9",
+});
+
+export const LogoMenu = styled(Box)({
   display: "flex",
   justifyContent: "center",
   alignItems: "center",
-  width: "100%",
+  width: "35px",
+  height: "35px",
+  margin: "7.5px",
+  padding: "7.5px",
+  borderRadius: "10px",
+  backgroundColor: "#4a7df6",
+  "& > svg": {
+    fill: "white", // Cambia el color del relleno del svg
+    width: "100%", // Ajusta el tamaño al del contenedor padre
+    height: "100%", // Ajusta la altura al contenedor
+  },
 });
 
-export const MenuIcon = styled(Icon)({
-  color: "white",
-  fontSize: "25px",
-  padding: "5px",
+export const MenuOption = styled(Box)({
+  display: "flex",
+  justifyContent: "flex-start",
+  alignItems: "center",
+  width: "calc(100% - 25px)",
+  height: "25px",
+  margin: "12.5px 12.5px 20px 12.5px",
+  // ICON STYLES
+  "& > svg": {
+    fill: "white",
+    width: "25px",
+    height: "25px",
+  },
+  // TEXT STYLES
+  "& > span": {
+    marginLeft: "1rem",
+    fontWeight: "500",
+    fontSize: "0.8rem",
+  },
 });
--- src/components/Menu/SideMenu.tsx
+++ src/components/Menu/SideMenu.tsx
@@ -1,17 +1,48 @@
 import { useState } from "react";
-import { MenuIcon, MenuOption, SideMenuContainer } from "./SideMenu.styled";
+import {
+  LogoMenu,
+  MenuHeader,
+  MenuHr,
+  MenuOption,
+  MenuSection,
+  SideMenuContainer,
+} from "./SideMenu.styled";
+import {
+  HomeOutlined as HomeIcon,
+  SettingsOutlined as SettingsIcon,
+  InfoOutlined as InfoIcon,
+  AcUnitOutlined as AcUnitIcon,
+} from "@mui/icons-material";
 
 export const SideMenu = () => {
-  const [expandedMenu, setExpandedMenu] = useState(false);
+  const [expandedMenu, setExpandedMenu] = useState(true);
+
+  const icons = [
+    { id: 1, component: <HomeIcon />, label: "Home" },
+    { id: 2, component: <SettingsIcon />, label: "Settings" },
+    { id: 3, component: <InfoIcon />, label: "Info" },
+  ];
+
   return (
     <SideMenuContainer
       active={expandedMenu}
-      onMouseEnter={() => setExpandedMenu(true)}
-      onMouseLeave={() => setExpandedMenu(false)}
+      onMouseEnter={() => setExpandedMenu(false)}
+      onMouseLeave={() => setExpandedMenu(true)}
     >
-      <MenuOption>
-        <MenuIcon className="fas fa-home" />
-      </MenuOption>
+      <MenuHeader>
+        <LogoMenu>
+          <AcUnitIcon />
+        </LogoMenu>
+      </MenuHeader>
+      <MenuSection>
+        {icons.map((icon) => (
+          <MenuOption key={icon.id}>
+            {icon.component}
+            {expandedMenu && <span>{icon.label}</span>}
+          </MenuOption>
+        ))}
+      </MenuSection>
+      <MenuHr />
     </SideMenuContainer>
   );
 };
